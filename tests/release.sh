528356cb89f8814d796b558f26b472ea3e8ea2fd
saku0512 <saku0512sec@gmail.com>
2026-08-29 14:02:51 +0900

    build(release): v0.16.0成果物matrixへ更新する

--- tests/release.sh
+++ tests/release.sh
@@ -87,7 +87,7 @@ for value in \
     "runner: windows-2025" \
     "name: Run platform tests" \
     "run: go test ./..." \
-    'scripts/package-gui.sh dist/package v0.15.0 "$GITHUB_RUN_NUMBER"' \
+    'scripts/package-gui.sh dist/package v0.16.0 "$GITHUB_RUN_NUMBER"' \
     'bash scripts/verify-gui-package.sh dist/package'
 do
     require_ci "$value"
